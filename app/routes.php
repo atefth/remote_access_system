7e099b09740aa9e372fc756104d15847db02f95b
atefth <atefth@gmail.com>
2014-08-13 03:42:58 +0600

    blocked site

--- app/routes.php
+++ app/routes.php
@@ -11,24 +11,24 @@
 |
 */
 
-Route::get('/', function()
-{
-	$page = 'home';
-	return View::make('home')->with('page', $page);
-});
+// Route::get('/', function()
+// {
+// 	$page = 'home';
+// 	return View::make('home')->with('page', $page);
+// });
 
-Route::get('control', function()
-{
-	$page = 'control';
-	$status = array();
-	$status[0] = Record::status(1);
-	$status[1] = Record::status(2);
-	$status[2] = Record::status(3);
-	$status[3] = Record::status(4);
-	$status[4] = Record::status(5);
-	$status[5] = Record::status(6);
-	return View::make('control')->with('page', $page)->with('status', $status);
-});
+// Route::get('control', function()
+// {
+// 	$page = 'control';
+// 	$status = array();
+// 	$status[0] = Record::status(1);
+// 	$status[1] = Record::status(2);
+// 	$status[2] = Record::status(3);
+// 	$status[3] = Record::status(4);
+// 	$status[4] = Record::status(5);
+// 	$status[5] = Record::status(6);
+// 	return View::make('control')->with('page', $page)->with('status', $status);
+// });
 
 Route::get('seed', function ()
 {
