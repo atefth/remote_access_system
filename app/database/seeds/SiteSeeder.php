a41b6871644f79c982d35b850080f66ff6ce9a58
Atef Haque <atefth@gmail.com>
2014-09-19 04:10:08 +0600

    updated site seeder

--- app/database/seeds/SiteSeeder.php
+++ app/database/seeds/SiteSeeder.php
@@ -5,8 +5,8 @@ class SiteSeeder extends Seeder {
     public function run()
     {
         DB::table('sites')->delete();
-        for ($i=0; $i < 10; $i++) { 
-            for ($j=0; $j < 10; $j++) { 
+        for ($i=0; $i < 5; $i++) { 
+            for ($j=0; $j < 5; $j++) { 
                 Site::create(array(
                 'name' => 'BTS_'.$i.$j
                 ));
