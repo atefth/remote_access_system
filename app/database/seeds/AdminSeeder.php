45f90eec9aa060c636edb0c411dec5f0e52c65da
atefth <atefth@gmail.com>
2014-11-15 13:50:41 +0600

    updated seeders

--- app/database/seeds/AdminSeeder.php
+++ app/database/seeds/AdminSeeder.php
@@ -4,16 +4,16 @@ class AdminSeeder extends Seeder {
 
     public function run()
     {
-        // DB::table('admins')->delete();
-        // Admin::create(array(
-        // 	'name' => 'SuperUser',
-        // 	'username' => 'root', 
-        // 	'password' => Hash::make('pass')
-        // 	));
-        // Admin::create(array(
-        // 	'name' => 'RobiTemp', 
-        // 	'username' => 'robi', 
-        // 	'password' => Hash::make('robi')));
+        DB::table('admins')->delete();
+        Admin::create(array(
+        	'name' => 'SuperUser',
+        	'username' => 'root', 
+        	'password' => Hash::make('pass')
+        	));
+        Admin::create(array(
+        	'name' => 'RobiTemp', 
+        	'username' => 'robi', 
+        	'password' => Hash::make('robi')));
         Admin::create(array(
          'name' => 'Base Technologies',
          'username' => 'base', 
